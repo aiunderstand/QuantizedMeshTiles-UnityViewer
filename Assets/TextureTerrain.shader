ab60e33ba91fd0bbc2609517f772b33bd9422111
Steven Bos <steven@stevenbos.com>
2017-01-31 12:13:00 +0100

    first!

---
+++ Assets/TextureTerrain.shader
@@ -0,0 +1,91 @@
+﻿Shader "Custom/TextureTerrain" {
+	Properties {
+		_MainTex ("Base (RGB) Alpha (A)", 2D) = "white" {}
+		_Color ("Main Color", Color) = (1,1,1,1)
+		_SplatControl ("Control", Color) = (0, 0, 0, 0)
+		_Splat1 ("Splat1", 2D) = "white" {}
+		_Splat2 ("Splat2", 2D) = "white" {}
+		_Splat3 ("Splat3", 2D) = "white" {}
+		_Splat4 ("Splat4", 2D) = "white" {}
+		_Mask ("Mask", 2D) = "white" {}
+		[HideInInspector] _Cutoff ("Alpha cutoff", Range(0,1)) = 0.5
+	}
+	
+	SubShader {
+		//Tags { "Queue"="AlphaTest" "RenderType"="Transparent" }
+		Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+		Blend SrcAlpha OneMinusSrcAlpha
+        LOD 200
+		
+		Cull Front
+        CGPROGRAM
+        #pragma surface surf Lambert vertex:vert alpha
+ 
+        sampler2D _MainTex;
+ 
+        struct Input {
+            float2 uv_MainTex;
+        };
+ 
+        // Flip normal for back faces
+        void vert (inout appdata_full v) {
+            v.normal *= -1;
+         }
+		 
+		   // Red back faces
+         void surf (Input IN, inout SurfaceOutput o) {
+             half4 c = tex2D (_MainTex, IN.uv_MainTex);
+             o.Albedo = fixed3(1,0,0);
+             //o.Alpha = c.a;
+			 o.Alpha = 0.3;
+         }
+         ENDCG
+
+		 Cull Back
+         CGPROGRAM
+
+		#pragma surface surf Lambert alphatest:_Cutoff
+
+		sampler2D _Splat1;
+		sampler2D _Splat2;
+		sampler2D _Splat3;
+		sampler2D _Splat4;
+		sampler2D _Mask;
+		fixed4 _Color;
+		fixed4 _SplatControl;
+			 
+		struct Input {
+			float2 uv_Splat1;
+		};
+		
+		void surf (Input IN, inout SurfaceOutput o) {
+			fixed3 c = _Color.rgb;
+			if (_SplatControl.r != 0)
+			{
+				fixed4 tmp = tex2D(_Splat1, IN.uv_Splat1);
+				c = lerp(c, tmp.rgb, _SplatControl.r * tmp.a);
+			}
+			if (_SplatControl.g != 0)
+			{
+				fixed4 tmp = tex2D(_Splat2, IN.uv_Splat1);
+				c = lerp(c, tmp.rgb, _SplatControl.g * tmp.a);
+			}
+			if (_SplatControl.b != 0)
+			{
+				fixed4 tmp = tex2D(_Splat3, IN.uv_Splat1);
+				c = lerp(c, tmp.rgb, _SplatControl.b * tmp.a);
+			}
+			if (_SplatControl.a != 0)
+			{
+				fixed4 tmp = tex2D(_Splat4, IN.uv_Splat1);
+				c = lerp(c, tmp.rgb, _SplatControl.a * tmp.a);
+			}
+			o.Albedo =  c;
+			fixed4 b = tex2D(_Mask, IN.uv_Splat1);
+			o.Alpha = b.a;
+		}
+		ENDCG
+	}
+
+	Fallback "VertexLit"
+}
---
+++ Assets/viewer.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.IO.Compression;
+using Terrain.Tiles;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class viewer : MonoBehaviour {
+
+    public int X = 0;
+    public int Y = 0;
+    public int Z = 0;
+    public Material mat1;
+    public Material mat2;
+    //public bool showVertices = false;
+    private List<GameObject> tiles = new List<GameObject>();
+
+    public void CreateTile()
+    {
+        StartCoroutine(requestTile(0,0,0));
+        StartCoroutine(requestTile(0,1,0));
+    }
+
+    public void SwitchMaterial()
+    {
+        foreach (var t in tiles)
+        {
+            if (t.GetComponent<MeshRenderer>().material == mat1)
+                t.GetComponent<MeshRenderer>().material = mat2;              
+            else
+                t.GetComponent<MeshRenderer>().material = mat1;
+        }
+    }
+    private IEnumerator requestTile(int x, int y, int z)
+    {
+        string url = "http://assets.agi.com/stk-terrain/v1/tilesets/world/tiles/" + x + "/" + y + "/" + z + ".terrain";
+
+        DownloadHandlerBuffer handler = new DownloadHandlerBuffer();
+        TerrainTile terrainTile;
+        UnityWebRequest http = new UnityWebRequest(url);
+        http.downloadHandler = handler;
+        yield return http.Send();
+
+        if (!http.isError)
+        {
+            //get data
+            MemoryStream stream = new MemoryStream(http.downloadHandler.data);
+
+            //unzip
+            var deflateStream = new GZipStream(stream, CompressionMode.Decompress);
+
+            //parse into tile
+            terrainTile = TerrainTileParser.Parse(deflateStream);
+
+            //create tile 
+            var tile = new GameObject("tile x:" + x + " y:" + y + " z:" + z);
+            tile.AddComponent<MeshFilter>().mesh = terrainTile.GetMesh(x, y, z);
+
+
+            if (y==0)
+                tile.AddComponent<MeshRenderer>().material = mat1;
+            else
+                tile.AddComponent<MeshRenderer>().material = mat2;
+
+
+            tile.transform.localScale = new Vector3(0.5f, -0.003f, 1);
+            tile.transform.position = new Vector3(x - (y * 180), 0, 0);
+            tiles.Add(tile);
+            
+            ////create vertices (if needed)
+            //if (showVertices)
+            //{
+            //    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //    Mesh cubeMesh = cube.GetComponent<MeshFilter>().mesh;
+            //    cube.SetActive(false);
+
+            //    var vert = tile.GetComponent<MeshFilter>().mesh.vertices;
+
+            //    for (int i = 0; i < vert.Length; i++)
+            //    {
+            //        GameObject computePoint = new GameObject(name) { hideFlags = HideFlags.HideInHierarchy };
+            //        computePoint.transform.parent = tile.transform;
+            //        computePoint.transform.position = new Vector3(vert[i].x, vert[i].y, vert[i].z);
+            //        computePoint.AddComponent<MeshFilter>().mesh = cubeMesh;
+            //        computePoint.AddComponent<MeshRenderer>().material = mat2;
+            //        computePoint.AddComponent<MeshCollider>();
+            //    }
+            //}
+        }
+        else
+        {
+            Debug.Log("Error loading tile");
+        }
+    }
+}
